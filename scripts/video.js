6580bcad31c839eaaef0de009749e7ee99825637
Sifat Hossain <sifathossain1326@gmail.com>
2025-07-26 00:17:57 +0530

    Profile Picture

--- scripts/video.js
+++ scripts/video.js
@@ -42,7 +42,7 @@ async function loadVideos() {
     }
 };
 
-const cardDemo = {
+/* const cardDemo = {
     "category_id": "1003",
     "video_id": "aaac",
     "thumbnail": "https://i.ibb.co/NTncwqH/luahg-at-pain.jpg",
@@ -60,7 +60,7 @@ const cardDemo = {
     },
     "description": "Comedian Kevin Hart brings his unique brand of humor to life in 'Laugh at My Pain.' With 1.1K views, this show offers a hilarious and candid look into Kevin's personal stories, struggles, and triumphs. It's a laugh-out-loud experience filled with sharp wit, clever insights, and a relatable charm that keeps audiences coming back for more."
 }
-
+ */
 const displayVideos = (data) => {
     const videoContainer = document.getElementById('videos');
     data.forEach(item => {
@@ -71,14 +71,15 @@ const displayVideos = (data) => {
             <figure class = "h-[200px]">
                 <img
                 src=${item.thumbnail}
-                class = "h-full w-full object-cover"
+                class = "h-full w-full object-cover rounded-lg"
                 alt="Videos" />
             </figure>
-            <div class="card-body">
-                <h2 class="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+            <div class="px-0 py-4 flex">
+                <div>
+                    <img class = "h-10 w-10 object-cover rounded-full" src = ${item.authors[0].profile_picture} />
+                </div>
+
+                <div>
                 </div>
             </div>
         `;
