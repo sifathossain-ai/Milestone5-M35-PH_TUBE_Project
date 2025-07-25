fe0306c5776675c26d2601aa879643f8f7b57bfb
Sifat Hossain <sifathossain1326@gmail.com>
2025-07-25 22:45:46 +0530

    Add button

--- scripts/video.js
+++ scripts/video.js
@@ -1,1 +1,31 @@
 console.log("Hello!")
+
+// 1 - Fetch, Load & Show Categories on HTML
+
+// Create LoadCategories
+
+const loadCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch(error => console.log("ERROR IS: ", error))
+};
+
+// Create DisplayCategories
+
+const displayCategories = (data) => {
+    const categoryDisplay = document.getElementById('categories');
+    for(const item of data){
+        console.log(item);
+
+        // Create Button
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = item.category;
+
+        // Add button to display
+        categoryDisplay.appendChild(button)
+    }
+}
+
+loadCategories();
